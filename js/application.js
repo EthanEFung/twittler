08814788b640f7470e6989f21f0d34a030e3e52d
Ethan Fung <EthanEFung@gmail.com>
2017-06-02 15:55:57 -0700

    Change Twittle Stream while loop
    Changed the while loop to a for loop.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
   // var $body = $('body');
   // $body.html('');
+  var $twittlerStream = $('.twittler-stream')
+
   var index = streams.home.length - 1;
-    while(index >= 0){
+
+    for(index; index >= 0; index--){
       var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($body);
-      index -= 1;
+      var $tweet = $('<div class="tweet"></div>');
+
+      var $user = $('<span class="user" href="stream.user"></span>');
+      var $message = $('<p class="message"></p>');
+      var $time = $('<span class="time"></span>');
+      $tweet.append($('<b>@</b>').append($user.text(tweet.user)), ' posted on: ', $time.text(tweet.created_at), $message.text(tweet.message), );
+      $tweet.appendTo($twittlerStream);
     }
+
+  function renderUsersTwittlerStream(user) {
+
+  }
+  
+  function showNewTwittles() {
+
+  }
+
+  
+
 });
