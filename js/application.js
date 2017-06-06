b9fad088e33905428bdbfd9c55cb6ce6c77b588d
Ethan Fung <EthanEFung@gmail.com>
2017-06-05 17:18:27 -0700

    Make twittle-box create tweets
    created a form that allows users to enter
    a string of characters in a input box, and
    submit the data.  The button creates a 'tweet'
    object, which is pushed into the users stream.
    Then then obj is used to prepend to the twitter
    stream in the DOM.

--- js/application.js
+++ js/application.js
@@ -1,29 +1,63 @@
 $(document).ready(function(){
-  // var $body = $('body');
-  // $body.html('');
-  var $twittlerStream = $('.twittler-stream')
+  $('.twittler-stream').html('');
 
-  var index = streams.home.length - 1;
+  const $twittlerStream = $('.twittler-stream');
 
-    for(index; index >= 0; index--){
-      var tweet = streams.home[index];
-      var $tweet = $('<div class="tweet"></div>');
+  let index = streams.home.length - 1;
 
-      var $user = $('<span class="user" href="stream.user"></span>');
-      var $message = $('<p class="message"></p>');
-      var $time = $('<span class="time"></span>');
-      $tweet.append($('<b>@</b>').append($user.text(tweet.user)), ' posted on: ', $time.text(tweet.created_at), $message.text(tweet.message), );
-      $tweet.appendTo($twittlerStream);
+  streams.users.user = [];
+
+  function domReady(tweet) {
+
+    let $user = $('<a href="#">@</a>').wrap('<span class="user"></span>').append(tweet.user);
+    let $time = $('<span class="created-at"></span>').append(tweet.created_at);
+    let $message = $('<p class="message"></p>').append(tweet.message);
+
+    return $('<div class="tweet"></div>').append($user, ' ', $time, $message);
+  }   
+
+  function renderTwittlerStream() {
+    for(index; index >= 0; index--) {
+      let tweet = streams.home[index];
+      domReady(tweet).appendTo($twittlerStream);
     }
+  }  
 
-  function renderUsersTwittlerStream(user) {
-
-  }
-  
-  function showNewTwittles() {
-
+  function addTweet(message) {
+    
   }
 
+  function createTweet() {
+    let tweet = {}
+    tweet.created_at = new Date().getUTCDate();
+    tweet.message = $('#twittle-box-input').val();
+    tweet.user = "user";
+    return tweet;
+  }
   
+
+  //Users should be able to enter characters into a the user entry box, and upon clicking or pressing enter
+  //should be able to display tweet as the first item on the stream.
+    //this should append the tweet to their own stream
+
+
+  $('#twittle-box-submit').on('click', function() {
+    let tweet = createTweet();
+    streams.users.user.push(tweet);
+    console.log(streams.users.user);
+    domReady(tweet).prependTo($twittlerStream);
+    //select the text entered in the twittle-box-input
+    //create a tweet with the users name, and time created
+    //append the tweet to the users stream
+    //
+  });
+
+  // $('#refresh-button').on('click', function() {
+  //   renderTwittlerStream();
+  //   //refresh button should remove all the tweets from the existing stream, and replace them with new
+  //   //tweets
+  // });
+
+renderTwittlerStream();  
 
 });
