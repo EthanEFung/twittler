18f022e8925251bbaae6e97a07c7c8c9a623b8c3
Ethan Fung <EthanEFung@gmail.com>
2017-06-09 10:20:27 -0700

    Refactor application.js for empathy
    Attempting to refactor code to have functions
    become more readable and for DRY.

--- js/application.js
+++ js/application.js
@@ -1,6 +1,13 @@
 
 $('.twittler-stream').html('');
 
+function Tweet() {
+  let tweet = {};
+  tweet.created_at = $.timeago(new Date).toLocaleString();
+  tweet.message = $('#twittle-box-input').val();
+  tweet.user = 'visitor';
+  return tweet;
+}
 
 function domReady(tweet) {
 
@@ -11,41 +18,61 @@ function domReady(tweet) {
   return $('<div class="tweet"></div>').append($user, ' ', $time, $message);
 }   
 
-function renderTwittlerStream() {
+function restrictStreamLength(index, number) {
+  return (index > number) ? number : index;
+}
 
-  let index = streams.home.length - 1;
-  if(index > 10) { index = 10; }
+function renderTwittlerStream(stream, index) {
+
+  index = restrictStreamLength(index, 10);
+  
   for(index; index >= 0; index--) {
-    let tweet = streams.home[index];
+
+    let tweet = stream[index];
     tweet.created_at = tweet.created_at.toLocaleString();
     domReady(tweet).appendTo('.twittler-stream');
+    
   }
-  jQuery("time.timeago").timeago();
+  $("time.timeago").timeago();
+}
+
+function renderHomeStream() {
+
+  let stream = streams.home;
+  let index = stream.length - 1;
+
+  console.log('index after twittler-stream gens on home stream', index);
+  renderTwittlerStream(stream, index);
+
+}
+
+function renderUserStream() {
+  console.log('whats this?', this)
+
+  let tag = $(this).text().slice(1);
+  let stream = streams.users[tag];
+  let index = stream.length - 1;
+
+  console.log('index after twittler-stream gens on user-button', index);
+  renderTwittlerStream(stream, index);
+
 }
 
 function resetTwittlerStream() {
-
   $('.twittler-stream').children().remove();
-
 }
 
-function Tweet() {
-  let tweet = {};
-  tweet.created_at = $.timeago(new Date).toLocaleString();
-  tweet.message = $('#twittle-box-input').val();
-  tweet.user = 'visitor';
-  return tweet;
-}
+
+
+
   
 
 $(document).ready(function(){
-
 
   streams.users.visitor = [];
 
   renderTwittlerStream();
   
-
   $('#twittle-box-submit').on('click', function() {
     let tweet = new Tweet;
     streams.users.visitor.push(tweet);
@@ -56,27 +83,17 @@ $(document).ready(function(){
 
   $('.twittler-stream').on('click','#user-button', function() {
 
-    let tag = $(this).text().slice(1)    
-
-    let index = streams.users[tag].length - 1
-    if (index > 10) { index = 10 }
-
     resetTwittlerStream();
-    
-    for(index; index >= 0; index--) {
-      let tweet = streams.users[tag][index];
-      tweet.created_at = tweet.created_at.toLocaleString();
-      domReady(tweet).appendTo('.twittler-stream');
-      $("time.timeago").timeago();
-    }
+    renderUserStream();
+   
 
   });
       
 
   $('#refresh-button').on('click', function() {
     
-    resetTwittlerStream();    
-    renderTwittlerStream();
+    resetTwittlerStream();
+    renderHomeStream();
 
   }) 
 
